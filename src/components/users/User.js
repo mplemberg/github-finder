c14a271937e4e14fd426adf9d231e0cc4342f85c
Mark <mplemberg@gmail.com>
2019-11-05 14:42:25 -0800

    repos moved to context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,13 @@
 import React, { useEffect, Fragment, useContext } from "react";
 import Spinner from "../layout/Spinner";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Repos from "../repos/Repos";
 import GithubContext from "../../context/github/githubContext";
 
-const User = ({ repos, getUserRepos, match }) => {
+const User = ({ match }) => {
   const githubContext = useContext(GithubContext);
 
-  const { getUser, loading, user } = githubContext;
+  const { getUser, loading, user, getUserRepos, repos } = githubContext;
 
   useEffect(() => {
     getUser(match.params.login);
@@ -102,8 +101,4 @@ const User = ({ repos, getUserRepos, match }) => {
   );
 };
 
-User.propTypes = {
-  getUserRepos: PropTypes.func.isRequired,
-  repos: PropTypes.array.isRequired
-};
 export default User;
